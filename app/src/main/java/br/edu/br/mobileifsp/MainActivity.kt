465ebc785dc6f07a9695dc314fa8e39fcb5cc7ca
Gabriel Giacometti <gabrielhdfg460@gmail.com>
2024-03-13 11:16:00 -0300

    Adicionando o botão de enviar

--- app/src/main/java/br/edu/br/mobileifsp/MainActivity.kt
+++ app/src/main/java/br/edu/br/mobileifsp/MainActivity.kt
@@ -1,20 +1,30 @@
 package br.edu.br.mobileifsp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtName: TextView
+    lateinit var btnSend: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        edtName = findViewById(R.id.edt_nome)
+        btnSend = findViewById(R.id.btn_send)
+
+        btnSend.setOnClickListener(View.OnClickListener {
+            val nome: String = edtName.text.toString()
+            Toast.makeText(this, getString(R.string.message) +nome,Toast.LENGTH_SHORT).show()
+        })
+
     }
 }
